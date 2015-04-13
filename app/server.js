13746d32c178797e40c1cb3c37ddf8de14c3ae5b
Patrick Fieger <patrickfieger90@gmail.com>
2015-04-13 13:48:23 +0200

    port as variable

--- app/server.js
+++ app/server.js
@@ -3,7 +3,8 @@ var express = require('express')
   , dbconfig = require('./config/database')
   , expressconfig = require('./config/express')(app)
   , passportconfig = ('./config/passport')
-  , routes = require('./app/routes')(app);
+  , routes = require('./app/routes')(app)
+  , port = 3000
 
   app.use(express.static(__dirname + '/public')); 
-  app.listen(3000, function() {console.log('Express server listening on port 80');});
+  app.listen(port, function() {console.log('Express server listening on port ' + port);});
